12962679ef13b9128348c18327e70f8d62310c84
Coenraad Human <coen.human@gmail.com>
2020-04-08 11:04:10 +0200

    Left over changes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AllStatsTableComponent } from './pages/all-stats-table/all-stats-table.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TimelineComponent } from './pages/timeline/timeline.component';
-import {SouthAfricaPageComponent} from './pages/south-africa-page/south-africa-page.component';
+import { SouthAfricaPageComponent } from './pages/south-africa-page/south-africa-page.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'timeline/:country', component: TimelineComponent },
   { path: 'table-all-locations', component: AllStatsTableComponent },
-  { path: 'south-africa', component: SouthAfricaPageComponent},
+  { path: 'south-africa', component: SouthAfricaPageComponent },
   { path: '**', redirectTo: '' },
 ];
 
